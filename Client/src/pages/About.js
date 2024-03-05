0b77b6cabef3b9f0a9a2bea6561ceb8d8b7effd4
HoneyMarsmatics <honey@marsmatics.com>
2024-03-05 13:30:23 -0500

    about us add something tag

--- Client/src/pages/About.js
+++ Client/src/pages/About.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const About=()=>{
     return <><h1>my About us page</h1>
-        <b>The mscito branch</b></>
+        <b>The mscito branch</b>
+        <b>added Something</b>
+        </>
 }
 
 export default About;
